ca3a7777e087dffeb25412643870be08dd1e2f92
Meshcheryakova <lmavi.kisune@gmail.com>
2023-08-30 17:57:50 +0400

    Exercise_4(Lesson_4)

---
+++ srt/main/java/Lesson_4/MainApp.java
@@ -0,0 +1,15 @@
+package Lesson_4;
+
+public class MainApp {
+    public static void main(String[] args) {
+        Print printABC= new Print();
+
+        Thread threadA = new Thread(printABC::printA);
+        Thread threadB = new Thread(printABC::printB);
+        Thread threadC = new Thread(printABC::printC);
+
+        threadA.start();
+        threadB.start();
+        threadC.start();
+    }
+}
